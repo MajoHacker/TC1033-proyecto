24ba48c489739c09a4f731886b756ddee7853e71
MajoHacker <111139800+MajoHacker@users.noreply.github.com>
2022-11-10 11:51:14 -0600

    Update Partidos.h

--- Partidos.h
+++ Partidos.h
@@ -2,13 +2,15 @@ class Partidos{
     private:
         int numero_partidos;
         int zona;
+        int personas;
     public:
-    Partidos(int num_partidos, int zon){
+    Partidos(int num_partidos, int zon, int pers){
         numero_partidos = num_partidos;
         zona = zon;
+        personas = pers;
     }
     int resultado_partidos(){
-        return numero_partidos*zona;
+        return numero_partidos*zona*personas;
     }
     // Getters
     int getNumero_partidos(){
@@ -17,11 +19,17 @@ class Partidos{
     int getZona(){
         return zona;
     }
+    int getPersonas(){
+        return personas;
+    }
     // Setters 
     void setNumero_partidos(int num_partidos){
         numero_partidos = num_partidos;
     }
     void setZona(int zon){
         zona = zon;
+    }
+    void setPersonas(int pers){
+        personas = pers;
     }
 };
