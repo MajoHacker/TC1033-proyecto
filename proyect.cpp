e8536fac0a4b2ac05ee2ccc5b78895f22063c1b3
MajoHacker <111139800+MajoHacker@users.noreply.github.com>
2022-11-10 11:49:22 -0600

    Update proyect.cpp

--- proyect.cpp
+++ proyect.cpp
@@ -6,18 +6,32 @@ using namespace std;
 # include "Partidos.h"
 
 int main(){
-    Avion p1(2,1000);
-    cout<<"El costo de vuelo es: " <<p1.resultado_vuelo()<<endl;
+    int num_pasajero;
+    int precio_vuelo;
+    cout<<"¿Cuántos pasajeros?\n"; cin>>num_pasajero;
+    cout<<"¿Costo por boleto?\n"; cin>>precio_vuelo;
+    Avion p1(num_pasajero,precio_vuelo);
+    cout<<"El costo de vuelo ida y vuelta es: " <<p1.resultado_vuelo()<<endl;
 
-    Hotel p2(3,9,3400);
+    int num_habitacion;
+    int num_noches;
+    int precio_noche;
+    cout<<"¿Cuántas habitaciones?\n"; cin>>num_habitacion;
+    cout<<"¿Cuántas noches?\n"; cin>>num_noches;
+    cout<<"¿Costo por noche de cada habitación?\n"; cin>>precio_noche;    
+    Hotel p2(num_habitacion,num_noches,precio_noche);
     cout<<"El costo de hotel es: "<<p2.resultado_hotel()<<endl;
 
-    Partidos p3(3,7800);
+    int num_partidos;
+    int costo_boleto;
+    cout<<"¿Cuántos partidos?\n"; cin>>num_partidos;
+    cout<<"¿Precio por boleto?\n"; cin>>costo_boleto;    
+    Partidos p3(num_partidos,costo_boleto,num_pasajero);
     cout<<"El costo de partidos es: "<<p3.resultado_partidos()<<endl;
 
     int total;
     total=p1.resultado_vuelo()+p2.resultado_hotel()+p3.resultado_partidos();
-    cout<<"El costo total del viaje es: "<<total<<endl;
-    
+    cout<<"\nEl costo total del viaje es: "<<total<<endl;
+
     return 0;
-}
+}   
