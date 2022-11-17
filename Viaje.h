515ef4299bd3bca4de9c7a5fc8782b80524a44c1
MajoHacker <111139800+MajoHacker@users.noreply.github.com>
2022-11-17 17:45:23 -0600

    Update Viaje.h

--- Viaje.h
+++ Viaje.h
@@ -4,11 +4,11 @@
 
 class Viaje{
     private: 
-    Avion resultado_vuelo;
-    Hotel resultado_hotel;
-    Partidos resultado_partidos;
+    int resultado_vuelo;
+    int resultado_hotel;
+    int resultado_partidos;
     public:
-    Viaje(Avion r_v, Hotel r_h, Partidos r_p){
+    Viaje(int r_v, int r_h, int r_p){
         resultado_vuelo=r_v;
         resultado_hotel=r_h;
         resultado_partidos=r_p;
@@ -17,23 +17,23 @@ class Viaje{
         return resultado_vuelo+resultado_hotel+resultado_partidos;
     }
     // Getters
-    Avion getResultado_vuelo(){
+    int getResultado_vuelo(){
         return resultado_vuelo;
     }
-    Hotel getResultado_hotel(){
+    int getResultado_hotel(){
         return resultado_hotel;
     }
-    Partidos getResultado_partidos(){
+    int getResultado_partidos(){
         return resultado_partidos;
     }
     // Setters
-    void setResultado_vuelo(Avion r_v){
+    void setResultado_vuelo(int r_v){
         resultado_vuelo=r_v;
     }
-    void setResultado_hotel(Hotel r_h){
+    void setResultado_hotel(int r_h){
         resultado_hotel=r_h;
     }
-    void setResultado_partidos(Partidos r_p){
+    void setResultado_partidos(int r_p){
         resultado_partidos=r_p;
     }
 };
