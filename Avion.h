46fa0e369d462cadca6031f43e24444f7ca3e8e6
MajoHacker <111139800+MajoHacker@users.noreply.github.com>
2022-11-28 10:49:55 -0600

    Update Avion.h

--- Avion.h
+++ Avion.h
@@ -1,27 +1,27 @@
 class Avion{
-    private: 
+    protected: 
         int pasajeros;
         int tipo_vuelo;
     public:
-    Avion (int pasajero, int tipo_vue){
-        pasajeros = pasajero;
-        tipo_vuelo = tipo_vue;
-    }
-    int resultado_vuelo(){
-        return pasajeros*tipo_vuelo*2;
-    }
-    // Getters
-    int getPasajeros(){
-        return pasajeros;
-    }
-    int getTipo_vuelo(){
-        return tipo_vuelo;
-    }
-    // Setters 
-    void setPasajeros(int pasajero){
-        pasajeros = pasajero;
-    }
-    void setTipo_vuelo(int tipo_vue){
-        tipo_vuelo = tipo_vue;
-    }
+        Avion (int pasajero, int tipo_vue){
+            pasajeros = pasajero;
+            tipo_vuelo = tipo_vue;
+        }
+        int resultado_vuelo(){
+            return pasajeros*tipo_vuelo*2;
+        }
+        // Getters
+        int getPasajeros(){
+            return pasajeros;
+        }
+        int getTipo_vuelo(){
+            return tipo_vuelo;
+        }
+        // Setters 
+        void setPasajeros(int pasajero){
+            pasajeros = pasajero;
+        }
+        void setTipo_vuelo(int tipo_vue){
+            tipo_vuelo = tipo_vue;
+        }
 };
