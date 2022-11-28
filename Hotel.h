41c6351ab004ebd657af24e7690c7a446ee9ec97
MajoHacker <111139800+MajoHacker@users.noreply.github.com>
2022-11-28 10:50:29 -0600

    Update Hotel.h

--- Hotel.h
+++ Hotel.h
@@ -4,32 +4,32 @@ class Hotel{
         int noches;
         int tipo_habitacion;
     public:
-    Hotel(int habitacion, int noche, int tipo){
-        habitaciones = habitacion;
-        noches = noche;
-        tipo_habitacion = tipo;
-    }
-    int resultado_hotel(){
-        return habitaciones*noches*tipo_habitacion;
-    }
-    // Getters
-    int getHabitaciones(){
-        return habitaciones;
-    }
-    int getNoches(){
-        return noches;
-    }
-    int getTipo_habitacion(){
-        return tipo_habitacion;
-    }
-    // Setters 
-    void setHabitaciones(int habitacion){
-        habitaciones = habitacion;
-    }
-    void setNoches(int noche){
-        noches = noche;
-    }
-    void setTipo_habitacion(int tipo){
-        tipo_habitacion = tipo;
-    }
+        Hotel(int habitacion, int noche, int tipo){
+            habitaciones = habitacion;
+            noches = noche;
+            tipo_habitacion = tipo;
+        }
+        int resultado_hotel(){
+            return habitaciones*noches*tipo_habitacion;
+        }
+        // Getters
+        int getHabitaciones(){
+            return habitaciones;
+        }
+        int getNoches(){
+            return noches;
+        }
+        int getTipo_habitacion(){
+            return tipo_habitacion;
+        }
+        // Setters 
+        void setHabitaciones(int habitacion){
+            habitaciones = habitacion;
+        }
+        void setNoches(int noche){
+            noches = noche;
+        }
+        void setTipo_habitacion(int tipo){
+            tipo_habitacion = tipo;
+        }
 };
