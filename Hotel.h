d0c288a43daddfe906b7b6bba6581a119ebb3707
MajoHacker <111139800+MajoHacker@users.noreply.github.com>
2022-12-01 11:43:21 -0600

    Update Hotel.h

--- Hotel.h
+++ Hotel.h
@@ -1,14 +1,18 @@
+// Crear la clase Hotel 
 class Hotel{
     private:
+        // Declarar los atributos de la clase
         int habitaciones;
         int noches;
         int tipo_habitacion;
     public:
+        // Constructor de la clase
         Hotel(int habitacion, int noche, int tipo){
             habitaciones = habitacion;
             noches = noche;
             tipo_habitacion = tipo;
         }
+        // Métodos 
         int resultado_hotel(){
             return habitaciones*noches*tipo_habitacion;
         }
