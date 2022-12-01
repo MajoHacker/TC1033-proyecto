f5dc5de388c9b8261a79ea531c11b921ef924a6f
MajoHacker <111139800+MajoHacker@users.noreply.github.com>
2022-12-01 11:45:27 -0600

    Update Viaje.h

--- Viaje.h
+++ Viaje.h
@@ -1,18 +1,22 @@
+// Incluir las clases Tipo, Avion y Partidos (composición)
 # include "Tipo.h"
 # include "Hotel.h"
 # include "Partidos.h"
-
+// Crear la clase viaje 
 class Viaje{
     private: 
+        // Declarar los atributos de la clase 
         int resultado_vuelo;
         int resultado_hotel;
         int resultado_partidos;
     public:
+        // Constructor
         Viaje(int r_v, int r_h, int r_p){
             resultado_vuelo=r_v;
             resultado_hotel=r_h;
             resultado_partidos=r_p;
         }
+        // Métodos
         int costo_viaje(){
             return resultado_vuelo+resultado_hotel+resultado_partidos;
         }
